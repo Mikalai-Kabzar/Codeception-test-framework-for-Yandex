42154c8db6c76cf5f2ba46db1cd837d743386a5e
Mikalai Kabzar <Mikalai_Kabzar@epam.com>
2017-12-27 16:46:31 +0300

    add autotests

---
+++ tests/acceptance/Yandex/navigation/MainPageNavigationButtonsExistCest.php
@@ -0,0 +1,47 @@
+<?php
+
+use Pages\MainPage as MainPage;
+
+class MainPageNavigationButtonsExistCest {
+
+    function _before(YandexTester $I)
+    {
+        $I->wantTo('Check existence of basic web-element on main page of Yandex.');
+        $I->amOnPage('/');
+    }
+
+    function checkImagesExists(YandexTester $I)
+    {
+        $I->seeElement(MainPage::IMAGES_XPATH);
+    }
+
+    function checkMapsExists(YandexTester $I)
+    {
+        $I->seeElement(MainPage::MAPS_XPATH);
+    }
+
+    function checkMarketExists(YandexTester $I)
+    {
+        $I->seeElement(MainPage::MARKET_XPATH);
+    }
+
+    function checkMusicExists(YandexTester $I)
+    {
+        $I->seeElement(MainPage::MUSIC_XPATH);
+    }
+
+    function checkTranslateExists(YandexTester $I)
+    {
+        $I->seeElement(MainPage::TRANSLATE_XPATH);
+    }
+
+    function checkVideoExists(YandexTester $I)
+    {
+        $I->seeElement(MainPage::VIDEO_XPATH);
+    }
+
+    function checkNewsExists(YandexTester $I)
+    {
+        $I->seeElement(MainPage::NEWS_XPATH);
+    }
+}
