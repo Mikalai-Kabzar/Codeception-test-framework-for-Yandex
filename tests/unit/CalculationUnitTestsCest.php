942a01484be615c4f6f3a935b5c744bb94398297
Mikalai Kabzar <Mikalai_Kabzar@epam.com>
2017-12-28 12:14:02 +0300

    add data providers and unit tests

---
+++ tests/unit/CalculationUnitTestsCest.php
@@ -0,0 +1,66 @@
+<?php
+use \Codeception\Example as Example;
+class CalculationUnitTestsCest
+{
+    function checkEqualPositive1(UnitTester $I)
+    {
+        $I->assertEquals('1', '1');
+    }
+
+    function checkEqualPositive2(UnitTester $I)
+    {
+        $I->assertEquals('1', '1');
+    }
+
+    function checkEqualNegative1(UnitTester $I)
+    {
+        $I->assertEquals('1', '2', 'It is a negative test for string.');
+    }
+
+    /**
+     * @dataProvider providerAdd1
+     */
+    function checkEqualNegative2(UnitTester $I, Example $example)
+    {
+        $I->amGoingTo('Break the system');
+        $I->comment('Some comment');
+        $I->assertEquals($example['expected'], $example['actual'], 'It is a negative test for numbers.');
+    }
+
+    /**
+     * @dataProvider providerAdd2
+     * @param UnitTester $I
+     * @param \Codeception\Example $example
+     */
+    function checkEqualNegative3(UnitTester $I, Example $example)
+    {
+        $I->amGoingTo('Break the system');
+        $I->comment('Some comment');
+        $I->assertEquals($example[0], $example[1], 'It is a negative test for numbers.');
+    }
+
+    /**
+     * @return array
+     */
+    public function providerAdd1()
+    {
+        return array(
+            ['expected' => 1, 'actual' => 2],
+            ['expected' => 10, 'actual' => 10],
+            ['expected' => -3, 'actual' => -3]
+        );
+    }
+
+    public function providerAdd2()
+    {
+        return array(
+            [1, 2],
+            [101, 10],
+            ['123', '321'],
+            [101, 10],
+            ['Mikalai', 'Mikalai'],
+            [ -3, -13]
+        );
+    }
+
+}
